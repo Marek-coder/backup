a443c1d7e5b43b0bc4b075d6eea6c2f9a13c70d1
Marek Molinski <chbevi95@gmail.com>
2022-04-05 21:19:45 +0200

    drugi-commit

--- js/script.js
+++ js/script.js
@@ -18,10 +18,10 @@ function geoFindMe() {
      }
      
      // Creating a map object
-     var map = new L.map('map', mapOptions);
+     var map = L.map('map', mapOptions);
      
      // Creating a Layer object
-     var layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+     var layer = L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
      
      // Adding layer to the map
      map.addLayer(layer);
